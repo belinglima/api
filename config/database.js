4ec79805b338736c539da7aa181e6437bfdea092
Salomão Lima <belinglima@gmail.com>
2019-06-15 20:19:59 -0300

    Ajuste do banco

--- config/database.js
+++ config/database.js
@@ -6,6 +6,9 @@ const Env = use('Env')
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 
+const Url = require('url-parse')
+const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
+
 module.exports = {
   /*
   |--------------------------------------------------------------------------
@@ -46,16 +49,16 @@ module.exports = {
   | Here we define connection settings for MySQL database.
   |
   | npm i --save mysql
-  |
+  | edited to complete the issue for work fine. by Salomão Lima
   */
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', '')
+      host: Env.get('DB_HOST', 'DATABASE_URL.hostname'),
+      port: Env.get('DB_PORT', DATABASE_URL.port),
+      user: Env.get('DB_USER', DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
     },
     debug: Env.get('DB_DEBUG', false)
   },
